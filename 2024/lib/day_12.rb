c8472474c8c647c52a2f1058766675df91b8d724
Chris Sturm <chris.sturm@fidelitylife.com>
2024-12-20 20:23:52 -0800

    day 12 part 2 wip ex 4 not working

--- 2024/lib/day_12.rb
+++ 2024/lib/day_12.rb
@@ -20,16 +20,16 @@ class Day12
   def self.as_plot(i, j, grid)
     rows = grid.rows
     cols = grid.cols
-    return nil if i.negative? || i >= rows || j.negative? || j >= cols
+    return if i.negative? || i >= rows || j.negative? || j >= cols
 
-    input = grid.input
     n = (i * cols) + j + 1
-    e = ((rows + 1) * cols) + ((cols + 1) * i) + j + 2
+    e = ((rows + 1) * cols) + ((j + 1) * rows) + i + 1
     s = ((i + 1) * cols) + j + 1
-    w = ((rows + 1) * cols) + ((cols + 1) * i) + j + 1
+    w = ((rows + 1) * cols) + (j * rows) + i + 1
+
     sides = [n, e, s, w]
     plot = (cols * i) + j
-    type = input[i][j]
+    type = grid.input[i][j]
     [type, plot, sides]
   end
 
@@ -38,15 +38,13 @@ class Day12
       next if neighbor.nil? || neighbor[0] != type || visited.include?(neighbor[1])
 
       visited << neighbor[1]
-
       area[0] << neighbor[1]
       area[1] += neighbor[2]
       walk_region(neighbor[1] / grid.rows, neighbor[1] % grid.cols, grid, type, area, visited)
     end
   end
 
-  def self.calculate_cost(grid)
-    costs = Hash.new(0)
+  def self.walk(grid)
     areas = []
     rows = grid.rows
     cols = grid.cols
@@ -63,7 +61,11 @@ class Day12
         walk_region(i, j, grid, type, area, visited)
       end
     end
+    areas
+  end
 
+  def self.calculate_cost(areas)
+    costs = Hash.new(0)
     areas.each do |type, area|
       num_a = area[0].length
       num_p = area[1].tally.select { |_p, n| n == 1 }
@@ -75,12 +77,46 @@ class Day12
   end
 
   def self.part_1(input)
-    puts "#{input}"
+    # puts "#{input}"
     grid = parse(input)
-    calculate_cost(grid)
+    areas = walk(grid)
+    calculate_cost(areas)
   end
 
-  def self.part_2(_input)
-    0
+  def self.calculate_cost_2(areas, grid)
+    costs = Hash.new(0)
+    areas.each do |type, area|
+      num_a = area[0].length
+      perimeter = area[1].tally.select { |_p, n| n == 1 }.map { |p| p[0] }
+      n_s, e_w = perimeter.partition { |p| p > grid.rows * grid.cols }
+      n_s.sort!
+      e_w.sort!
+      # puts "#{n_s} #{e_w}"
+      sides = []
+      [[n_s, grid.rows], [e_w, grid.cols]].each do |a, edge|
+        sides << [a.shift]
+        until a.empty?
+          last_s = sides.last.last
+          if last_s + 1 == a.first && last_s % edge != 0
+            sides.last << a.first
+          else
+            sides << [a.first]
+          end
+          a.shift
+          puts "#{sides}"
+        end
+      end
+      num_s = sides.length
+      puts "cost: #{type}: #{num_a} * #{num_s} = #{num_a * num_s}"
+      costs[[type, area[0]]] = num_a * num_s
+    end
+
+    costs.values.sum
+  end
+
+  def self.part_2(input)
+    grid = parse(input)
+    areas = walk(grid)
+    calculate_cost_2(areas, grid)
   end
 end
--- 2024/lib/day_12_test.rb
+++ 2024/lib/day_12_test.rb
@@ -22,23 +22,13 @@ class Day12Test < Minitest::Test
       MMMISSJEEE
     INPUT
 
-    @test_input = @test_input.strip
-
-    @input = get_input(12)
-  end
-
-  def test_part_1_example
-    @example = <<~EXAMPLE
+    @example_1 = <<~EXAMPLE
       AAAA
       BBCD
       BBCC
       EEEC
     EXAMPLE
 
-    assert_equal 140, Day12.part_1(@example)
-  end
-
-  def test_part_1_example_2
     @example_2 = <<~INPUT
       OOOOO
       OXOXO
@@ -47,10 +37,56 @@ class Day12Test < Minitest::Test
       OOOOO
     INPUT
 
+    @example_3 = <<~INPUT
+      EEEEE
+      EXXXX
+      EEEEE
+      EXXXX
+      EEEEE
+    INPUT
+
+    @example_4 = <<~INPUT
+      AAAAAA
+      AAABBA
+      AAABBA
+      ABBAAA
+      ABBAAA
+      AAAAAA
+    INPUT
+
+    @test_input = @test_input.strip
+    @example_1 = @example_1.strip
+    @example_2 = @example_2.strip
+    @example_3 = @example_3.strip
+    @example_4 = @example_4.strip
+
+    @input = get_input(12)
+  end
+
+  def test_as_plot
+    grid = Day12.parse("AA\nBB\n")
+    # puts "#{grid}"
+    # (0..(grid.rows - 1)).each do |i|
+    #   (0..(grid.cols - 1)).each do |j|
+    #     print "|#{Day12.as_plot(i, j, grid)}| "
+    #   end
+    #   puts
+    # end
+
+    assert_equal ['A', 0, [1, 9, 3, 7]], Day12.as_plot(0, 0, grid)
+    assert_equal ['B', 3, [4, 12, 6, 10]], Day12.as_plot(1, 1, grid)
+  end
+
+  def test_part_1_example_1
+    assert_equal 140, Day12.part_1(@example_1)
+  end
+
+  def test_part_1_example_2
     assert_equal 772, Day12.part_1(@example_2)
   end
 
   def test_part_1
+    skip
     assert_equal 1930, Day12.part_1(@test_input)
 
     answer = Day12.part_1(@input)
@@ -59,9 +95,41 @@ class Day12Test < Minitest::Test
     submit_answer(12, answer, 1)
   end
 
+  def test_part_2_example_1
+    puts
+    puts 'part 2 ex 1'
+    puts "#{@example_1}"
+    puts
+    assert_equal 80, Day12.part_2(@example_1)
+  end
+
+  def test_part_2_example_2
+    puts
+    puts 'part 2 ex 2'
+    puts "#{@example_2}"
+    puts
+    assert_equal 436, Day12.part_2(@example_2)
+  end
+
+  def test_part_2_example_3
+    puts
+    puts 'part 2 ex 3'
+    puts "#{@example_3}"
+    puts
+    assert_equal 236, Day12.part_2(@example_3)
+  end
+
+  def test_part_2_example_4
+    puts
+    puts 'part 2 ex 4'
+    puts "#{@example_4}"
+    puts
+    assert_equal 368, Day12.part_2(@example_4)
+  end
+
   def test_part_2
-    skip('Part 2 not yet implemented')
-    assert_equal 1, Day12.part_2(@test_input)
+    skip
+    assert_equal 1206, Day12.part_2(@example_3)
 
     answer = Day12.part_2(@input)
     puts "Part 2: #{answer}"
